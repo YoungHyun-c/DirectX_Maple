2df7ee08b0777f119cb969ff9c6011593a2d7df0
young <dudgus614@naver.com>
2023-10-11 01:17:54 +0900

    홍영현 : 버텍스 구조 변경, 엔진에서 제공하는 버텍스는 GameEngineVertex로 변경

--- DirecX_Maple/GameEngineCore/GameEngineDevice_Resources.cpp
+++ DirecX_Maple/GameEngineCore/GameEngineDevice_Resources.cpp
@@ -3,7 +3,7 @@
 #include "GameEngineTransform.h"
 
 #include "GameEngineVertex.h"
-#include "GameEngineVerTexBuffer.h"
+#include "GameEngineVertexBuffer.h"
 #include "GameEngineIndexBuffer.h"
 #include "GameEngineShader.h"
 #include "GameEngineRasterizer.h"
@@ -18,34 +18,12 @@
 
 void GameEngineDevice::ResourcesInit()
 {
-	// 엔진수준에서 지원해주는 가장 기초적인 리소스들은 여기에서 만들어질 것이다.
-	// 기본 매쉬
-	// 기본 텍스처
-	// 기본 쉐이더
-	// 여기에서 자기 텍스처 로드하지않기.
-
-	//{
-	//	// 엔진용 쉐이더를 전부다 로드하는 코드를 친다.
-	//	GameEngineDirectory Dir;
-	//	Dir.MoveParentToExistsChild("GameEngineCoreShader");
-	//	Dir.MoveChild("GameEngineCoreShader");
-	//	std::vector<GameEngineFile> Files = Dir.GetAllFile({ ".fx" });
-
-	//	for (size_t i = 0; i < Files.size(); i++)
-	//	{
-	//		//GameEngineVertexShader::Load(Files[i].GetStringPath(), "ColorShader_VS");
-	//		// 구조적으로 잘 이해하고 있는지를 자신이 명확하게 인지하기 위해서 (ex)File을 한번에 삭제, File관리
-	//		GameEngineFile& File = Files[i];
-	//		GameEngineShader::AutoCompile(File);
-	//	}
-	//}
-
 	{
-		// 엔진용 쉐이더를 전부다 로드하는 코드를 친다.
+		// 엔진용 쉐이더를 전부다 전부다 로드하는 코드를 친다.
 		GameEngineDirectory Dir;
 		Dir.MoveParentToExistsChild("GameEngineResources");
 		Dir.MoveChild("GameEngineResources");
-		Dir.MoveChild("Texture");
+		Dir.MoveChild("Textrure");
 		std::vector<GameEngineFile> Files = Dir.GetAllFile();
 
 		for (size_t i = 0; i < Files.size(); i++)
@@ -56,13 +34,15 @@ void GameEngineDevice::ResourcesInit()
 		}
 
 		GameEngineSprite::CreateSingle("NSet.png");
+
 	}
 
+
 	{
-		std::vector<GameEngineVertex2D> Vertex;
+		std::vector<GameEngineVertex> Vertex;
 		Vertex.resize(4 * 6);
 
-		GameEngineVertex2D BaseVertexs[4];
+		GameEngineVertex BaseVertexs[4];
 
 		BaseVertexs[0] = { { -0.5f, -0.5f, -0.5f, 1.0f } };
 		BaseVertexs[1] = { { 0.5f, -0.5f, -0.5f, 1.0f } };
@@ -107,22 +87,142 @@ void GameEngineDevice::ResourcesInit()
 		GameEngineVertexBuffer::Create("Box", Vertex);
 	}
 
+
+	// Sphere
+		// 스피어
 	{
-		std::vector<GameEngineVertex2D> Vertex;
+		GameEngineVertex V;
+		std::vector<GameEngineVertex> VBVector;
+		std::vector<UINT> IBVector;
+
+		float Radius = 0.5f;
+		// 북극점부터 시작합니다.
+		V.POSITION = float4(0.0f, Radius, 0.0f, 1.0f);
+		V.TEXCOORD = float4(0.5f, 0.0f);
+		// 노말 백터 혹은 법선백터라고 불리며
+		// 면에 수직인 벡터를 의미하게 된다.
+		// 빛을 반사할때 필수.
+		V.NORMAL = float4(0.0f, Radius, 0.0f, 1.0f);
+		V.NORMAL.Normalize();
+		V.NORMAL.W = 0.0f;
+		V.TANGENT = float4(1.0f, 0.0f, 0.0f, 0.0f);
+		V.BINORMAL = float4(0.0f, 0.0f, 1.0f, 0.0f);
+
+		VBVector.push_back(V);
+
+		UINT iStackCount = 16; // 가로 분할 개수입니다.
+		UINT iSliceCount = 16; // 세로분할 개수
+
+		float yRotAngle = GameEngineMath::PI / (float)iStackCount;
+		float zRotAngle = (GameEngineMath::PI * 2) / (float)iSliceCount;
+
+		// UV의 가로세로 간격값을 구한다.
+		float yUvRatio = 1.0f / (float)iStackCount;
+		float zUvRatio = 1.0f / (float)iStackCount;
+
+		for (UINT y = 1; y < iStackCount; ++y)
+		{
+			// 각 간격에 대한 각도값
+			float phi = y * yRotAngle;
+			for (UINT z = 0; z < iSliceCount + 1; ++z)
+			{
+				float theta = z * zRotAngle;
+				V.POSITION = float4{
+					Radius * sinf(y * yRotAngle) * cosf(z * zRotAngle),
+					Radius * cosf(y * yRotAngle),
+					Radius * sinf(y * yRotAngle) * sinf(z * zRotAngle),
+					1.0f // 위치 크기 값에 영향을 주기 위해서
+				};
+
+				// V.Pos *= GameEngineRandom::RandomFloat(-0.9f, 0.1f);
+
+				V.TEXCOORD = float4(yUvRatio * z, zUvRatio * y);
+				V.NORMAL = V.POSITION.NormalizeReturn();
+				V.NORMAL.W = 0.0f;
+
+				V.TANGENT.X = -Radius * sinf(phi) * sinf(theta);
+				V.TANGENT.Y = 0.0f;
+				V.TANGENT.Z = Radius * sinf(phi) * cosf(theta);
+				V.TANGENT = V.TANGENT.NormalizeReturn();
+				V.TANGENT.W = 0.0f;
+
+				V.BINORMAL = float4::Cross3D(V.TANGENT, V.NORMAL);
+				V.BINORMAL = V.BINORMAL.NormalizeReturn();
+				V.BINORMAL.W = 0.0f;
+
+
+				VBVector.push_back(V);
+			}
+		}
+
+		// 남극점
+		V.POSITION = float4(0.0f, -Radius, 0.0f, 1.0f);
+		V.TEXCOORD = float4(0.5f, 1.0f);
+		V.NORMAL = float4(0.0f, -Radius, 0.0f, 1.0f);
+		V.NORMAL.Normalize();
+		V.NORMAL.W = 0.0f;
+		V.TANGENT = float4(-1.0f, 0.0f, 0.0f, 0.0f);
+		V.BINORMAL = float4(0.0f, 0.0f, -1.0f, 0.0f);
+		VBVector.push_back(V);
+
+		// 인덱스 버퍼를 만듭니다.
+		IBVector.clear();
+
+		// 북극점을 이루는 점을 만드는건.
+		for (UINT i = 0; i < iSliceCount; i++)
+		{
+			// 시작은 무조건 북극점
+			IBVector.push_back(0);
+			IBVector.push_back(i + 2);
+			IBVector.push_back(i + 1);
+		}
+
+		for (UINT y = 0; y < iStackCount - 2; y++)
+		{
+			for (UINT z = 0; z < iSliceCount; z++)
+			{
+				IBVector.push_back((iSliceCount + 1) * y + z + 1);
+				IBVector.push_back((iSliceCount + 1) * (y + 1) + (z + 1) + 1);
+				IBVector.push_back((iSliceCount + 1) * (y + 1) + z + 1);
+
+				IBVector.push_back((iSliceCount + 1) * y + z + 1);
+				IBVector.push_back((iSliceCount + 1) * y + (z + 1) + 1);
+				IBVector.push_back((iSliceCount + 1) * (y + 1) + (z + 1) + 1);
+
+			}
+		}
+
+		// 마지막으로 남극점 인덱스
+		UINT iBotIndex = (UINT)VBVector.size() - 1;
+		for (UINT i = 0; i < iSliceCount; i++)
+		{
+			// 시작은 무조건 북극점
+			IBVector.push_back(iBotIndex);
+			IBVector.push_back(iBotIndex - (i + 2));
+			IBVector.push_back(iBotIndex - (i + 1));
+		}
+
+		GameEngineVertexBuffer::Create("Sphere", VBVector);
+		GameEngineIndexBuffer::Create("Sphere", IBVector);
+
+		GameEngineMesh::Create("Sphere");
+
+		std::shared_ptr<GameEngineMesh> Mesh = GameEngineMesh::Create("DebugSphere", "Sphere", "Sphere");
+	}
+
+
+	{
+		std::vector<GameEngineVertex> Vertex;
 		Vertex.resize(4);
 
-		// 이미지를 자르려면 TEXCORRD값이 바뀌어야 하는데,
+		// 이미지를 자르려면 TEXCOORD값이 바뀌어야 하는데.
 		Vertex[0] = { { -0.5f, 0.5f, 0.0f, 1.0f }, {0.0f, 0.0f} };
-		Vertex[1] = { { 0.5f, 0.5f, 0.0f, 1.0f }, {1.0f, 0.0f} };
-		Vertex[2] = { { 0.5f, -0.5f, 0.0f, 1.0f } , {1.0f, 1.0f} };
-		Vertex[3] = { { -0.5f, -0.5f, 0.0f, 1.0f }, {0.0f, 1.0f} };
-
-		//Vertex[0] = { { -1.0f, 1.0f, 0.0f, 1.0f }, {0.0f, 0.0f} };
-		//Vertex[1] = { { 1.0f, 1.0f, 0.0f, 1.0f }, {1.0f, 0.0f} };
-		//Vertex[2] = { { 1.0f, -1.0f, 0.0f, 1.0f } , {1.0f, 1.0f} };
-		//Vertex[3] = { { -1.0f, -1.0f, 0.0f, 1.0f }, {0.0f, 1.0f} };
+		Vertex[1] = { { 0.5f, 0.5f, 0.0f, 1.0f } , {1.0f, 0.0f} };
+		Vertex[2] = { { 0.5f, -0.5f, 0.0f, 1.0f }  , {1.0f, 1.0f} };
+		Vertex[3] = { { -0.5f, -0.5f, 0.0f, 1.0f } , {0.0f, 1.0f} };
 
 		GameEngineVertexBuffer::Create("Rect", Vertex);
+
 
 		std::vector<unsigned int> Index =
 		{
@@ -131,20 +231,21 @@ void GameEngineDevice::ResourcesInit()
 		};
 
 		GameEngineIndexBuffer::Create("Rect", Index);
-		
+
 		GameEngineMesh::Create("Rect");
 	}
 
 	{
-		std::vector<GameEngineVertex2D> Vertex;
+		std::vector<GameEngineVertex> Vertex;
 		Vertex.resize(4);
 
-		Vertex[0] = { { -1.0f, -1.0f, 0.0f, 1.0f}, {0.0f, 0.0f} };
-		Vertex[1] = { { 1.0f, -1.0f, 0.0f, 1.0f}, {1.0f, 0.0f} };
-		Vertex[2] = { { 1.0f, 1.0f, 0.0f, 1.0f}, {1.0f, 1.0f} };
-		Vertex[3] = { { -1.0f, 1.0f, 0.0f, 1.0f}, {0.0f, 1.0f} };
+		Vertex[0] = { { -1.0f, -1.0f, 0.0f, 1.0f }, {0.0f, 0.0f} };
+		Vertex[1] = { { 1.0f, -1.0f, 0.0f, 1.0f },  {1.0f, 0.0f} };
+		Vertex[2] = { { 1.0f, 1.0f, 0.0f, 1.0f },   {1.0f, 1.0f} };
+		Vertex[3] = { { -1.0f, 1.0f, 0.0f, 1.0f },  {0.0f, 1.0f} };
 
 		GameEngineVertexBuffer::Create("FullRect", Vertex);
+
 
 		std::vector<unsigned int> Index =
 		{
@@ -155,52 +256,42 @@ void GameEngineDevice::ResourcesInit()
 		GameEngineIndexBuffer::Create("FullRect", Index);
 	}
 
-	// 나중에 사라질 것임
 	{
-		// 약간위험할 수 있다.
-		// 그래픽카드에서의 바이트 패딩규칙과
-		// sizeof(TransformData) 바이트패딩 규칙이
-		// 달라서 그리기 다르다고 인식할 수 있다.
-		// 주의 해야 한다.
-		// 이제 없어짐
-		// GameEngineConstantBuffer::CreateAndFind(sizeof(TransformData), "TransformData");
-	}
 
-	{
-		// 렌더링 할 때 채우기 모드를 결정한다.
 		//D3D11_FILL_MODE FillMode;
+		// 랜더링 할때 채우기 모드를 결정한다.
 
-		// 외적했는데 Z방향이 어디냐?
-		//D3D11_CULL_MODE => 방향이 어디든 건져낸다.
-		//D3D11_CULL_BACK => Z가 앞쪽인 픽셀들은 안건져 낸다.
-		//D3D11_CULL_FRONT => Z가 뒤쪽인 픽셀들은 안건져 낸다.
-
+		// 외적했는데 z방향이 어디냐?
+		// D3D11_CULL_NONE => 방향이 어디든 건져낸다.
+		// D3D11_CULL_BACK => z가 앞쪽인 픽셀들은 안건져 낸다.
+		// D3D11_CULL_FRONT => z가 뒤쪽인 픽셀들은 안건져 낸다.
+		// 
 		// 0, 1, 2,
 		// 0, 2, 3
 		// 인덱스 버퍼의 그리는 순서와 연관이 크다.
 
 		// 이녀석은 인덱스 버퍼
 
-		/*D3D11_CULL_MODE CullMode;
-		BOOL FrontCounterClockwise;
-		INT DepthBias;
-		FLOAT DepthBiasClamp;
-		FLOAT SlopeScaleDepthBias;
-		BOOL DepthClipEnable;
-		BOOL ScissorEnable;
-		BOOL MulitsampleEnable;
-		BOOL AntialiasedLineEnable;*/
+		//D3D11_CULL_MODE CullMode;
+		//BOOL FrontCounterClockwise;
+		//INT DepthBias;
+		//FLOAT DepthBiasClamp;
+		//FLOAT SlopeScaledDepthBias;
+		//BOOL DepthClipEnable;
+		//BOOL ScissorEnable;
+		//BOOL MultisampleEnable;
+		//BOOL AntialiasedLineEnable;
 
 		D3D11_RASTERIZER_DESC Desc = {};
-		Desc.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID; // 픽셀에 닿은 전체 면적
-		//Desc.FillMode = D3D11_FILL_MODE::D3D11_FILL_WIREFRAME; // 픽셀에 닿은 테두리
+		Desc.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
 		Desc.CullMode = D3D11_CULL_MODE::D3D11_CULL_NONE;
-		//Desc.DepthClipEnable = TRUE; // 화면 바깥의 물체 그림 그릴지
+		// Desc.DepthClipEnable = TRUE;
 		std::shared_ptr<GameEngineRasterizer> Rasterizer = GameEngineRasterizer::Create("EngineRasterizer", Desc);
-
 	}
 
+
 	{
+
 		//D3D11_FILL_MODE FillMode;
 		// 랜더링 할때 채우기 모드를 결정한다.
 
@@ -232,18 +323,19 @@ void GameEngineDevice::ResourcesInit()
 		std::shared_ptr<GameEngineRasterizer> Rasterizer = GameEngineRasterizer::Create("EngineWireRasterizer", Desc);
 	}
 
+
 	{
 		D3D11_BLEND_DESC Desc = {};
 
-		// 이건 좀 느린데
-		// 깊이버퍼라는 것과 관련이 있다.
-		// 나중에 함.
-		//Desc.AlphaToCoverageEnable;
+		// 이건 좀 느린데.
+		// 깊이버퍼라는 것과 관련이 있습니다.
+		// 나중에 함
+		// Desc.AlphaToCoverageEnable
 
-		// 렌더타겟을 세팅하는것과 관련이 있는데.
-		// 렌더타겟은 한번에 8개를 세팅할 수 있다.
-		// 그걸 1개 이상을 세팅했을때 각기 다른 세팅을 너가 일일히 넣어줄거냐 라는 옵션이다.
-		// 안한다하면 0번째 꺼로 나머지 모두를 세팅한다.
+		// 랜더타겟을 세팅하는것과 관련이 있는데.
+		// 랜더타겟은 한번에 8개를 세팅할수 있다.
+		// 그걸 1개 이상을 세팅했을때 각기 다른 세팅을 니가 일일이 넣어줄거냐 라는 옵션입니다.
+		// 안한다하면 0번째걸로 나머지 모두를 세팅한다.
 		Desc.IndependentBlendEnable = false;
 
 		// 블랜드 켤거냐.
@@ -252,19 +344,19 @@ void GameEngineDevice::ResourcesInit()
 		// 색깔 전체를 대상으로 삼겠다.
 		Desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
 
-		// + 외엔 쓸일이 거의 없다.
+		// + 빼고 써본적이 없어요.
 		Desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
 
 		// https://learn.microsoft.com/ko-kr/windows/win32/api/d3d11/ne-d3d11-d3d11_blend
 
 		// src srcColor * src의 알파
-		// 1, 0, 0(, 1) * 1.0f;
-		Desc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA; // src펙터
+		// 1, 0, 0(, 1) * 1.0f
+		Desc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA; // src팩터
 
-		// src 1, 0, 0, 1 * ( 1- src의 알파)
+		// src 1, 0, 0, 1 * (1 - src의 알파)
 		Desc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 
-		// ONE으로 사용 ZERO도 있는데 일단은 원색깔 그대로 ONE (*1.0f)
+		// 
 		Desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
 		Desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
 		Desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
@@ -272,28 +364,17 @@ void GameEngineDevice::ResourcesInit()
 		std::shared_ptr<GameEngineBlend> Blend = GameEngineBlend::Create("AlphaBlend", Desc);
 	}
 
+
 	{
+
 		D3D11_SAMPLER_DESC Desc = {};
 		// 일반적인 보간형식 <= 뭉개진다.
-		// D3D11_FilTER_MIN_MAG_MIP_
-		// 그 밉맵에서 색상가져올떄 다 뭉개는 방식으로 가져오겠다.
-		//Desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		// D3D11_FILTER_MIN_MAG_MIP_
+		// 그 밉맵에서 색상가져올때 다 뭉개는 방식으로 가져오겠다.
 		Desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
-		//D3D11_TEXTURE_ADDRESS_CLAMP;
-		//D3D11_TEXTURE_ADDRESS_WRAP;
-		//D3D11_TEXTURE_ADDRESS_MIRROR;
-
 		Desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
 		Desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
 		Desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
-
-		/*Desc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
-		Desc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
-		Desc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;*/
-
-		/*Desc.AddressU = D3D11_TEXTURE_ADDRESS_MIRROR;
-		Desc.AddressV = D3D11_TEXTURE_ADDRESS_MIRROR;
-		Desc.AddressW = D3D11_TEXTURE_ADDRESS_MIRROR;*/
 
 		Desc.MipLODBias = 0.0f;
 		Desc.MaxAnisotropy = 1;
@@ -304,10 +385,12 @@ void GameEngineDevice::ResourcesInit()
 		std::shared_ptr<GameEngineSampler> Rasterizer = GameEngineSampler::Create("EngineBaseSampler", Desc);
 	}
 
+
 	{
+
 		D3D11_SAMPLER_DESC Desc = {};
 		// 일반적인 보간형식 <= 뭉개진다.
-		// D3D11_FILTER_MIN_MAG_MIP
+		// D3D11_FILTER_MIN_MAG_MIP_
 		// 그 밉맵에서 색상가져올때 다 뭉개는 방식으로 가져오겠다.
 		Desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 		Desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
@@ -323,13 +406,9 @@ void GameEngineDevice::ResourcesInit()
 		std::shared_ptr<GameEngineSampler> Rasterizer = GameEngineSampler::Create("POINT", Desc);
 	}
 
-	// 엔진수준에서 지원해주는 가장 기초적인 리소스들은 여기에서 만들어질 것이다.
-	// 기본 매쉬
-	// 기본 텍스처
-	// 기본 쉐이더
-	// 여기에서 자기 텍스처 로드 하지 않기
+
 	{
-		// 엔진용 쉐이더를 전부다 로드하는 코드를 친다.
+		// 엔진용 쉐이더를 전부다 전부다 로드하는 코드를 친다.
 		GameEngineDirectory Dir;
 		Dir.MoveParentToExistsChild("GameEngineCoreShader");
 		Dir.MoveChild("GameEngineCoreShader");
@@ -343,6 +422,7 @@ void GameEngineDevice::ResourcesInit()
 		}
 	}
 
+
 	{
 		std::shared_ptr<GameEngineMaterial> Mat = GameEngineMaterial::Create("2DTexture");
 		Mat->SetVertexShader("TextureShader_VS");
@@ -355,4 +435,14 @@ void GameEngineDevice::ResourcesInit()
 		Mat->SetPixelShader("DebugColor_PS");
 		Mat->SetRasterizer("EngineWireRasterizer");
 	}
+
+
+
+	// 엔진수준에서 지원해주는 가장 기초적인 리소스들은 여기에서 만들어질 겁니다.
+	// 기본 매쉬
+	// 기본 텍스처
+	// 기본 쉐이더
+	// 여기에서 자기 텍스처 로드하지마세요.
+
+
 }
--- DirecX_Maple/GameEngineCore/GameEngineVertex.h
+++ DirecX_Maple/GameEngineCore/GameEngineVertex.h
@@ -1,31 +1,11 @@
 #pragma once
 #include "GameEngineInputLayOut.h"
-// Input LayOut이란 점 1개의 구조(특성)이다. (크기, 위치, 색 등등)
-
-// 설명 : 얘가 버텍스 버퍼 정보를 제일 잘 알고 있어야 한다.
-class GameEngineVertex2D
+// Input LayOut이란 점 1개의 구조에요.
+class GameEngineVertex
 {
 public:
 	static GameEngineInputLayOutInfo VertexInfo;
 
-	// 내가 얼마든지 바꿀 수 있는 것들이다.
-public:
-	float4 POSITION;
-	float4 TEXCOORD;
-};
-
-//struct Vertex
-//{
-//	float4 Pos0 : POSITION0;
-//	float4 Pos1 : POSITION1;
-//	float4 COLOR : COLOR;
-//	float4 COLOR : TEXCOORD;
-//};
-
-class GameEngine3Dvertex
-{
-public:
-	static GameEngineInputLayOutInfo VertexInfo;
 public:
 	float4 POSITION;
 	float4 TEXCOORD;
