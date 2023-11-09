c181f15101fe7f868307c8dbd31cf494fb92caf2
young <dudgus614@naver.com>
2023-11-10 00:27:17 +0900

    홍영현 : 폰트 업데이트

--- DirecX_Maple/GameEngineContents/ContentsCore.cpp
+++ DirecX_Maple/GameEngineContents/ContentsCore.cpp
@@ -91,7 +91,7 @@ void ContentsCore::Start()
 	GameEngineCore::CreateLevel<TestLevel>("0.TestLevel");
 	//GameEngineCore::ChangeLevel("6.BossRewardLevel");
 	 
-	GameEngineCore::ChangeLevel("3_1.HuntLevel");
+	GameEngineCore::ChangeLevel("2.TownLevel");
 	
 	// 맵 에디터 테스트
 	//GameEngineCore::CreateLevel<MapEditorLevel>("MapEditor");
